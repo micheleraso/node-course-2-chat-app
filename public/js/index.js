e2c21fe393d81ee83a0b99c63c775a0d483d390c
Michele Raso <dev@micheleraso.com>
2018-02-18 23:27:36 +0100

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -11,32 +11,37 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
     console.log('newMessage', message);
     var formattedTime = moment(message.createAt).format('h:mm a')
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
-socket.on('newLocationMessage', function(message) {
-    var formattedTime = moment(message.createAt).format('h:mm a')
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
+socket.on('newLocationMessage', function (message) {
+    console.log('newLocationMessage', message);
+    var formattedTime = moment(message.createAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
-jQuery('#message-form').on('submit', function(e) {
+jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
     var messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextbox.val()
-    }, function() {
+    }, function () {
         messageTextbox.val('');
     });
 });
@@ -44,18 +49,18 @@ jQuery('#message-form').on('submit', function(e) {
 var locationButton = jQuery('#send-location')
 
 locationButton.on('click', function () {
-    if(!navigator.geolocation) {
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
-    
+
     locationButton.attr('disabled', 'disabled').text('Sending location...');
-    navigator.geolocation.getCurrentPosition(function(position){
+    navigator.geolocation.getCurrentPosition(function (position) {
         locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
-    }, function() {
+    }, function () {
         locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.')
     });
